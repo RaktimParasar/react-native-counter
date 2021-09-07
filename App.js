c3c0a9b03a12de67171a52658a45ff171425f4d5
RaktimParasar <raktimparasar96@gmail.com>
2021-09-07 16:13:49 +0530

    initial setup

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, world!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  text: {
+    fontSize: 30,
+    color: 'blue',
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
